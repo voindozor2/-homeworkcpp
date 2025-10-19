f234ad163c61e4d309eccde7e99d78a4310d32a1
Ильин Борис <boris.i.ilin@gazprombank.ru>
2025-10-19 16:41:29 +0300

    LESSON-2: Разделил тесты

---
+++ tests/tests.h
@@ -0,0 +1,27 @@
+﻿
+
+void push_ten_elements_max_size_is_not_twenty_test();
+
+void push_eleven_elements_max_size_is_twenty_test();
+
+void push_to_tail_int_sequence_is_equals_to_expected_test();
+
+void push_int_sequence_and_delete_three_elements_from_back_is_equals_to_expected_test();
+
+void operator_is_equal_to_another_container_test();
+
+void when_push_to_index_equal_to_expected_test();
+
+void delete_from_index_equal_to_expected_test();
+
+void constructor_initialize_test();
+
+void operator_compare_test();
+
+void constructor_copy_test();
+
+void constructor_with_sequence_test();
+
+void operator_brackets_test();
+
+void run_vector_tests();
